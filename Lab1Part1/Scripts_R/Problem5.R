e6f0d2c67528b3a829f5b73e4d77acacbe822b9b
Akshay993 <akshaytotal1993@gmail.com>
2018-02-06 17:16:34 -0500

    Scripts for Lab1 Part1. Note: These are only 5 scripts out of 8

---
+++ Lab1Part1/Scripts_R/Problem5.R
@@ -0,0 +1,12 @@
+data()
+attach(mpg)
+head(mpg)
+summary(mpg)
+#after analysis remove the data from the memory detach(mpg)
+
+detach(mpg)
+
+
+library (help=datasets) library(datasets)
+head(uspop)
+plot(uspop)
