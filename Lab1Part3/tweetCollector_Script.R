26177340ff6cce98921e09cccfab86fe4aef6daf
Muthu2093 <muthuvel@buffalo.edu>
2018-02-14 17:37:50 -0500

    Uploaded data for "Postively Tested flu Report" - Lab1Part2
    Uploaded data for "Postively Tested flu Report" - Lab1Part2

---
+++ Lab1Part3/tweetCollector_Script.R
@@ -0,0 +1,13 @@
+library(twitteR)
+library(httr)
+library(SchedulerR) 
+
+
+setwd("/Users/muthuvel/Documents/GitHub/Twitter-client-for-Data-Collection-and-Exploratory-Data-Analysis-/Lab1Part3/")
+
+api_key <- "VxJ6qp5XL3VTclBzMBsD1Ez1A"
+api_secret <- "owezT5IVRVG8nvkSHXxqq4t2McwPO6mxesJTGU2549yHTJbP8m"
+access_token <- "0AWt3nkBVvLlX7hbUFLl0fEqIKs47qUU7V5UnFWH"
+access_token_secret <- "qnaD0Pyp9jUXfwVb82RlSKikuvVi2MAWxp1J0mD1Fle4d"
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
