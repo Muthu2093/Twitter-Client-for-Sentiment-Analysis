8b94369088a55224540862afe24a1927203cc9c3
Muthu2093 <muthuvel@buffalo.edu>
2018-02-20 14:13:14 -0500

    Updated script for Postive_tested chart - working
    Updated script for Postive_tested chart - working

--- Lab1Part2/Scripts/postive_Tested.R
+++ Lab1Part2/Scripts/postive_Tested.R
@@ -1,23 +1,28 @@
-PT_data <- read.csv("../Data/Postive Tested/Workbook1.csv")
-head(TTM)
-tail(TTM)
+rm(list =ls())
 
 
-# Plot the basic frame of the stacked bar chart.
+setwd("/Users/muthuvel/Documents/GitHub/Twitter-client-for-Data-Collection-and-Exploratory-Data-Analysis-/Lab1Part2/Scripts")
+
+data <- read.csv("../Data/Postive Tested/Workbook1.csv")
+head(data)
+tail(data)
+data <- subset( data, select = -Total...Tested )
+data <- data[-c(18, 19, 20, 21, 22), ]
+data <- t(data)
+colnames(data) <- data[1, ] 
+data <- data[-1, ]
+
+label=c("W41","W41","W42","W43","W44","W45","W46","W47","W48","W49","W50","W51","W52","W01","W02","W03","W04")
+colnames(data)= label
+data <- melt(data)
+
 library(ggplot2)
-ggplot(data = PT_data, aes(x = Week, y = Total...Tested, fill = B.)) + 
-  geom_bar(stat="identity")
 
-# Flip the x axis and y axis so that t names of behavior can be seen
-# clearly.
-ggplot(data = TTM, aes(x = Type.of.Behavior, y = Sample.Size, fill = Stage.of.Change)) + 
-  geom_bar(stat="identity") + coord_flip()
+label=c("Type.of.disease", "Week", "Total")
+colnames(data) <- label
+ggplot(data, aes(x=Week, y=Total, fill=Type.of.disease)) + 
+  geom_bar(stat="identity") +
+  xlab("\n Weeks(2017: 41-52, 2018:01-04)") +
+  ylab("Total number of postive cases\n") +
+  theme_bw()
 
-# Reorder the chunks so that they represent the logical order of stage
-# progression.
-TTM$Stage.of.Change <- factor(TTM$Stage.of.Change, levels = c("Precontemplation", 
-                                                              "Contemplation", "Preparation", "Action", "Maintenance"))
-# Re-plot the chart under the newly assigned order by changing the 'fill'
-# command.
-ggplot(data = TTM, aes(x = Type.of.Behavior, y = Sample.Size, fill = factor(Stage.of.Change))) + 
-  geom_bar(stat="identity") + coord_flip()
