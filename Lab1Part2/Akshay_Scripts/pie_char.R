8a35004c7a5f5ad3bc0578bf5d01da043719cdc3
Akshay993 <akshaytotal1993@gmail.com>
2018-03-07 18:44:36 -0500

    New file added in LabPart2 for Pie Chart.. Not yet complete

---
+++ Lab1Part2/Akshay_Scripts/pie_char.R
@@ -0,0 +1,4 @@
+setwd("/Users/akshaychopra/Documents/Twitter-client-for-Data-Collection-and-Exploratory-Data-Analysis-/Lab1Part2/Akshay_Scripts")
+
+data<- read.csv("../Data/Influenza sub type pie charts/Genetic04.csv")
+#table(data$X.Sub.type)
