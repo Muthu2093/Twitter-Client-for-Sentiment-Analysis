6cca75821dde926331237ad02854b34d94dd32b6
Muthu2093 <muthuvel@buffalo.edu>
2018-03-09 18:03:58 -0500

    Correct Mortality Code - Working fully
    Correct Mortality Code - Working fully

--- Lab1Part2/Akshay_Scripts/pediatric_mortality.R
+++ Lab1Part2/Akshay_Scripts/pediatric_mortality.R
@@ -1,29 +1,23 @@
 rm(list =ls())
 library(ggplot2)
 
-setwd("/Users/akshaychopra/Documents/Twitter-client-for-Data-Collection-and-Exploratory-Data-Analysis-/Lab1Part2/Akshay_Scripts")
-
 Weekly_data<- read.csv("../Data/Number of Influenza-Associated Pediatric Deaths/PedFluDeath_WeeklyData.csv")
 
-x_axis <- data.frame(Weekly_data$WEEK.NUMBER[c(6*c(1:32))])
+x_axis <- Weekly_data$WEEK.NUMBER[c(6*c(1:32))]
 #x_axis=Weekly_data[1:20,]
 #x_axis=x_axis[c(2)]
 
 SeasonDeath_data<-read.csv("../Data/Number of Influenza-Associated Pediatric Deaths/Season_Deaths.csv")
 
 p<-ggplot(Weekly_data,aes(x=WEEK.NUMBER, y=PREVIOUS.WEEKS.DEATHS)) +
-  ggtitle("Number of influenza associated Pediatric Deaths
-           by Week of Death")+
-  labs(x="Week of Death", y="Number of deaths", fill=NULL)+
-  scale_x_discrete(labels=x_axis)
+  geom_bar(stat="identity", fill = "forestgreen", position="stack", colour="black") +
+  labs(x="Week of Death", y="Number of deaths", fill=NULL)
   
-  
-  
-  b<- p+ geom_bar(stat="identity", fill = "green", position="stack")
-  b<-b+scale_fill_brewer(palette = "Set1")
-  
-  
-  b<- b+ theme(axis.text.x=element_text(angle=60))
-  
-  b
+b<-p+scale_fill_brewer(palette = "Set1")
+b<- b+ theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+               axis.text.x=element_text(angle=90, size=8, face="bold", color="black"))
+b <- b + scale_y_continuous(limit = c(0, 30))+
+    scale_x_discrete(breaks=x_axis,
+                        labels=x_axis)
+b
   
---
+++ Lab1Part2/Akshay_Scripts/test_mort.R
@@ -0,0 +1,41 @@
+rm(list =ls())
+library(ggplot2)
+
+Weekly_data<- read.csv("../Data/Number of Influenza-Associated Pediatric Deaths/PedFluDeath_WeeklyData.csv")
+
+rawdata <- Weekly_data
+x_axis <- Weekly_data$WEEK.NUMBER[c(6*c(1:32))]
+#x_axis=Weekly_data[1:20,]
+#x_axis=x_axis[c(2)]
+
+SeasonDeath_data<-read.csv("../Data/Number of Influenza-Associated Pediatric Deaths/Season_Deaths.csv")
+
+Weekly_data <- subset(Weekly_data, select =-c(1))
+
+Data = melt(Weekly_data, id=c("WEEK.NUMBER"))
+
+p<-ggplot(Data,aes(x=WEEK.NUMBER, y=value, fill=variable)) +
+  geom_bar(stat="identity", position="stack", colour="black") +
+  scale_fill_manual(name = "Week of Death",
+                    labels = c("Deaths Reported Current Week", "Deaths Reported in Previous Week"),
+                    values = c("CURRENT.WEEK.DEATHS"="cyan", "PREVIOUS.WEEKS.DEATHS"="forestgreen")) +
+  labs(x="", y="Number of deaths", fill=NULL)
+
+#b<-p+scale_fill_brewer(palette = "Set1")
+b<- p+ theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                          axis.text.x=element_text(angle=90, size=8, face="bold", color="black"),
+                          legend.position= "bottom")
+b <- b + scale_y_continuous(limit = c(0, 30))+
+  scale_x_discrete(breaks=x_axis,
+                   labels=x_axis)+
+  annotate("text", x = 20, y = 20, label = "2014 -2015", size = 4)+
+  annotate("text", x = 20, y = 19, label = "Number of Deaths Reported = 148", size = 2)+
+  annotate("text", x = 70, y = 20, label = "2015 -2016", size = 4)+
+  annotate("text", x = 70, y = 19, label = "Number of Deaths Reported = 93", size = 2)+
+  annotate("text", x = 120, y = 20, label = "2016 -2017", size = 4)+
+  annotate("text", x = 120, y = 19, label = "Number of Deaths Reported = 110", size = 2)+
+  annotate("text", x = 170, y = 20, label = "2017 -2018", size = 4)+
+  annotate("text", x = 170, y = 19, label = "Number of Deaths Reported = 53", size = 2) +
+  
+  ggtitle("Number of Influenza-Associated Pediatric Deaths by the Week of Death: 2014-2015 Season to Present")
+b
