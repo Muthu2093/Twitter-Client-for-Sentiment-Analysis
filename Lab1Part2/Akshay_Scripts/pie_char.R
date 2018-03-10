4df4259bd00bb5c52157aae61c285467e7e8a1c2
Akshay993 <akshaytotal1993@gmail.com>
2018-03-09 22:06:17 -0500

    final

--- Lab1Part2/Akshay_Scripts/pie_char.R
+++ Lab1Part2/Akshay_Scripts/pie_char.R
@@ -10,23 +10,38 @@ mydata_1<- mydata_1[-c(1 )]
 
 ## Pie char 1
 bp_1<- ggplot(mydata_1, aes(x="", y=Number, fill= mydata_1$Genetic_Group))+
-  geom_bar(width = 1, stat = "identity") + guides(fill=FALSE) +
-  scale_fill_manual("legend", values = c("3C.2a" = "indianred1", "3C.2a1" = "mistyrose", "3C.3a" = "firebrick3"))
+  geom_bar(width = 1, stat = "identity") + #guides(fill=FALSE) +
+  scale_fill_manual("legend", values = c("3C.2a" = "indianred1", "3C.2a1" = "mistyrose", "3C.3a" = "firebrick3")) +
+  ggtitle("Influenza A(H3N2)") +
+  theme(plot.title=element_text(hjust=0.5, size = 8))+
+  xlab("")+
+  ylab("")
   
 
-pie_1 <- bp_1 + coord_polar("y", start=250) +  theme_minimal()+ theme(axis.text.y  = element_blank(), axis.text.x = element_blank(), axis.ticks = element_blank(), panel.grid = element_blank())+ theme_bw()
+pie_1 <- bp_1 + coord_polar("y", start=250) +  
+  theme_minimal()+ 
+  theme_bw()
 pie_1
+
+annotate("text", x = 20, y = 20, label = "2014 -2015", size = 4)
 
 mydata_2<- data[6,]
 mydata_2<- mydata_2[-c(1)]
 
 ## Pie chat 2
 bp_2<- ggplot(mydata_2, aes(x="", y=Number, fill= mydata_2$Genetic_Group))+
-  geom_bar(width=1, stat ="identity")+ guides(fill=FALSE) +
-  scale_fill_manual("legend", values = c("6B.1" = "navajowhite2"))
+  geom_bar(width=1, stat ="identity")+ #guides(fill=FALSE) +
+  scale_fill_manual("legend", values = c("6B.1" = "navajowhite2")) +
+  ggtitle("Influenza A(H1N1)pdm09") +
+  theme(plot.title=element_text(hjust=0.5, size = 8)) +
+  xlab("")+
+  ylab("")
   
   
-pie_2 <- bp_2 + coord_polar("y", start=250) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+ theme_bw()
+pie_2 <- bp_2 + coord_polar("y", start=250) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())+ 
+  theme_bw()
 pie_2
 
 mydata_3<- data[1:2,]
@@ -35,10 +50,17 @@ mydata_3<- mydata_3[-c(1 )]
 
 ## Pie chat 3
 bp_3<- ggplot(mydata_3, aes(x="", y=Number, fill= mydata_3$Genetic_Group))+
-  geom_bar(width = 1, stat = "identity")+ guides(fill=FALSE) +
-  scale_fill_manual("legend", values = c("V1A" = "darkolivegreen1", "V1A-2Del" = "darkolivegreen1"))
+  geom_bar(width = 1, stat = "identity")+ #guides(fill=FALSE) +
+  scale_fill_manual("legend", values = c("V1A" = "darkolivegreen1", "V1A-2Del" = "darkolivegreen1")) +
+  ggtitle("Influenza B Victoria") +
+  theme(plot.title=element_text(hjust=0.5, size = 8)) +
+  xlab("")+
+  ylab("")
   
-pie_3 <- bp_3 + coord_polar("y", start=550) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+ theme_bw()
+pie_3 <- bp_3 + coord_polar("y", start=550) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())+ 
+  theme_bw()
 pie_3
 
 mydata_4<- data[3,]
@@ -47,12 +69,18 @@ mydata_4<- mydata_4[-c(1 )]
 
 ## Pie chat 4
 bp_4<- ggplot(mydata_4, aes(x="", y=Number ,fill=mydata_4$Genetic_Group))+
-  geom_bar(width = 1, stat = "identity")+ guides(fill=FALSE) +
+  geom_bar(width = 1, stat = "identity")+ #guides(fill=FALSE) +
   scale_fill_manual("legend", values = c("Y3" = "olivedrab3"))+
-  ggtitle("Influenza B Yamagata")
+  ggtitle("Influenza B Yamagata") +
+  theme(plot.title=element_text(hjust=0.5, size = 8)) +
+  xlab("")+
+  ylab("")
 
 
-pie_4 <- bp_4 + coord_polar("y", start=0) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+ theme_bw()
+pie_4 <- bp_4 + coord_polar("y", start=0) + 
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) + 
+  theme_bw()
 pie_4 
 
 ## ## Pie chat 5 - Main chart
@@ -64,17 +92,16 @@ df <- data.frame(
 bp<- ggplot(df, aes(x="", y=value, fill=group))+
   geom_bar(width = 1, stat = "identity") +
   scale_fill_manual(values= c("Influenza A H3N2"="red","Influenza A (H1N1)pdm09"="forestgreen", "Influenza A(Subtype Unknown)" ="orange",
-                              "Influenza B Victoria"="yellow", "Influenza B Yamagata"= "green","Influenza B(lineage not determined)"="cyan"))
+                              "Influenza B Victoria"="yellow", "Influenza B Yamagata"= "green","Influenza B(lineage not determined)"="cyan")) +
+  ggtitle("Influenza Positive Specimens \nReported by US Public\n Health Laboratories") +
+  theme(plot.title=element_text(hjust=0.5, size = 8),
+        legend.position = "right") +
+  xlab("")+
+  ylab("")
 
-
-pie <- bp + coord_polar("y", start=0) +guides(fill=FALSE) + facet_grid(supp ~ .)
+a <- plot.new()
+pie <- bp + coord_polar("y", start=0) #+guides(fill=FALSE) 
 
 
 ## Grouping all charts into one using ggarrange
-ggarrange(pie, pie_1,pie_2,pie_3,pie_4, ncol=3, nrow=2)
-
-par(mfrow=c(2,2))
-pie_1
-pie_2
-pi_3
-pie_4
+ggarrange(pie, pie_1,pie_2,a ,pie_3,pie_4, ncol=3, nrow=2)
--- Lab1Part2/Akshay_Scripts/test_mort.R
+++ Lab1Part2/Akshay_Scripts/test_mort.R
@@ -1,5 +1,6 @@
 rm(list =ls())
 library(ggplot2)
+library(reshape)
 
 Weekly_data<- read.csv("../Data/Number of Influenza-Associated Pediatric Deaths/PedFluDeath_WeeklyData.csv")
 
@@ -28,13 +29,13 @@ b<- p+ theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor =
 b <- b + scale_y_continuous(limit = c(0, 30))+
   scale_x_discrete(breaks=x_axis,
                    labels=x_axis)+
-  annotate("text", x = 20, y = 20, label = "2014 -2015", size = 4)+
+  annotate("text", x = 20, y = 20, label = "2014 -2015", size = 3)+
   annotate("text", x = 20, y = 19, label = "Number of Deaths Reported = 148", size = 2)+
-  annotate("text", x = 70, y = 20, label = "2015 -2016", size = 4)+
+  annotate("text", x = 70, y = 20, label = "2015 -2016", size = 3)+
   annotate("text", x = 70, y = 19, label = "Number of Deaths Reported = 93", size = 2)+
-  annotate("text", x = 120, y = 20, label = "2016 -2017", size = 4)+
+  annotate("text", x = 120, y = 20, label = "2016 -2017", size = 3)+
   annotate("text", x = 120, y = 19, label = "Number of Deaths Reported = 110", size = 2)+
-  annotate("text", x = 170, y = 20, label = "2017 -2018", size = 4)+
+  annotate("text", x = 170, y = 20, label = "2017 -2018", size = 3)+
   annotate("text", x = 170, y = 19, label = "Number of Deaths Reported = 53", size = 2) +
   
   ggtitle("Number of Influenza-Associated Pediatric Deaths by the Week of Death: 2014-2015 Season to Present")
